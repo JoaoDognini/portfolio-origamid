b20e0d2d6633fc332f7711d089724d568f9b5c10
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2024-07-31 14:41:18 -0300

    Adição de seção de Projetos

--- script.js
+++ script.js
@@ -53,11 +53,49 @@ const listaFormacoes = [
 		titulo: 'Formação Full stack JavaScript: crie um projeto com React e Node.js',
 		horas: '28',
 		certificado: 'https://cursos.alura.com.br/user/joaodognini/degree-full-stack-react-node-js-455268/certificate'
+	},
+	{
+		titulo: 'UI Design para Iniciantes',
+		horas: '34',
+		certificado: 'https://www.origamid.com/certificate/01f42c23'
 	}
+]
 
+const listaProjetos = [
+	{
+		titulo: 'Clonexo',
+		descricao: 'Projeto feito para a gincana de férias da empresa onde meu primo trabalha, sendo uma cópia básica do jogo conexo.ws. ',
+		imagem: './assets/imgs-projetos/clonexo.jpg',
+		tecs: ['React', 'HTML', 'TypeScript', 'SCSS'],
+		links: {
+			repositorio: "https://github.com/JoaoDognini/conexo-clone",
+			deploy: "https://conexo-clone.vercel.app/"
+		},
+	},
+	{
+		titulo: 'Organo',
+		descricao: 'Site feito para curso da Alura, de forma simples para possibilitar a criação de equipes, podendo cadastrar novos usuários e classificar a equipe que fazem parte.',
+		imagem: './assets/imgs-projetos/organo.png',
+		tecs: ['React', 'HTML', 'TypeScript', 'SCSS'],
+		links: {
+			repositorio: "https://github.com/JoaoDognini/organo",
+			deploy: "https://organo-joao.vercel.app/"
+		},
+	},
+	{
+		titulo: 'Aluroni',
+		descricao: 'Site projeto do curso da Alura, utilizando uma interface simples para conhecer mais sobre o restaurante e também o cardápio, podendo buscar, filtrar e ordenar os pratos conforme requisitado.',
+		imagem: './assets/imgs-projetos/aluroni.png',
+		tecs: ['React', 'HTML', 'TypeScript', 'SCSS'],
+		links: {
+			repositorio: "https://github.com/JoaoDognini/aluroni",
+			deploy: "https://aluroni-joao.vercel.app"
+		},
+	}
 ]
 
 const listaCursos = document.getElementById('lista_cursos');
+const listaProjeto = document.getElementById('lista_projetos');
 
 function criaListaCursos() {
 	listaFormacoes.map(curso => {
@@ -75,4 +113,32 @@ function criaListaCursos() {
 	})
 }
 
+function criaListaProjetos() {
+	listaProjetos.map(projeto => {
+		listaProjeto.innerHTML += `
+			<li>
+				<div class="projetos_card">
+					<div class="projetos_card_topo">
+						<div class="projetos_card_topo_textos">
+							<h1>${projeto.titulo}</h1>
+							<p>${projeto.descricao}</p>
+						</div>
+						
+						<img src=${projeto.imagem} alt="Imagem do ${projeto.titulo}" />
+					</div>
+					<div class="projetos_card_inferior">
+						<div class="projetos_card_inferior_botoes">
+							<a class="botao_projeto" href=${projeto.links.deploy}>Deploy</a>
+							<a class="botao_projeto" href=${projeto.links.repositorio}>Repositório</a>
+						</div>
+						<ul class="projetos_card_inferior_lista_tecs">
+							${projeto.tecs.map(tec => `<li class="projetos_card_inferior_lista_tecs_item">${tec}</li>`).join('')}
+						</ul>
+				</div>
+			</li>
+		`
+	})
+}
+
+criaListaProjetos();
 criaListaCursos();
